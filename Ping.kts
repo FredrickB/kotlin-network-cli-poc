20e19d9229543108298064ea0ef58f43373f15c7
FredrickB <mail@fredrickb.com>
2020-08-16 21:36:43 +0200

    Started converting executor-service and threads to coroutines.
    Currently having problems joining results from parallell execution
    _back_ to a collection that can be returned in a blocking coroutine.

--- Ping.kts
+++ Ping.kts
@@ -1,6 +1,7 @@
 #!/usr/bin/env kscript
 
 //DEPS com.xenomachina:kotlin-argparser:2.0.7
+//DEPS org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8
 
 //INCLUDE Scanner.kt
 
@@ -10,4 +11,4 @@ val results = scan()
 
 println("Hosts which are up: ${results.map { it.ipAddress }}")
 
-println("Done scanning.")
+println("Done scanning")
--- Scanner.kt
+++ Scanner.kt
@@ -1,35 +1,63 @@
 //INCLUDE ScanResult.kt
 
+import kotlinx.coroutines.*
 import java.net.InetAddress
 import java.net.NetworkInterface
 import java.time.LocalDateTime
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
+import kotlin.system.measureTimeMillis
 
 fun ping(device: String): Boolean {
     val address = InetAddress.getByName(device)
     return address.isReachable(2000)
 }
 
-fun pingNetwork(broadcastAddress: String): List<ScanResult> {
+suspend fun test(addresses: List<String>): List<ScanResult> = coroutineScope {
+    addresses.map { ipAddress ->
+        println("Pinging device $ipAddress")
+        val deferred = async(Dispatchers.IO) {
+            ScanResult(ipAddress = ipAddress, up = ping(ipAddress), scannedAt = LocalDateTime.now())
+        }
+        println("Done pinging device")
+        deferred
+    }.awaitAll()
+}
+
+fun pingNetwork(broadcastAddress: String): List<ScanResult> = runBlocking {
     val endIpAddress = broadcastAddress.substring(broadcastAddress.length - 3, broadcastAddress.length)
     val ipStartRange = broadcastAddress.substring(0, broadcastAddress.length - 3)
 
     println("Pinging network with broadcast: $broadcastAddress")
 
-    val service = Executors.newCachedThreadPool()
+//    val service = Executors.newCachedThreadPool()
 
-    val results = IntRange(start = 1, endInclusive = endIpAddress.toInt())
-            .map { device ->
-                val ipAddress = "$ipStartRange$device"
-                Callable { ScanResult(ipAddress = ipAddress, up = ping(ipAddress), scannedAt = LocalDateTime.now()) }
-            }
-            .map { service.submit(it) }
-            .map { it.get() }
+    val addresses = IntRange(start = 1, endInclusive = endIpAddress.toInt()).map { "$ipStartRange$it" }
 
-    service.shutdown()
+    println("Bam!")
+    val results = test(addresses)
+    println("results: $results")
+//
+//    runBlocking {
+//        println("Bam!")
+//        val results = test(addresses)
+//        println("results: $results")
+//    }
 
-    return results
+    emptyList<ScanResult>()
+    // return emptyList<ScanResult>()
+//
+//    val results = IntRange(start = 1, endInclusive = endIpAddress.toInt())
+//            .map { device ->
+//                val ipAddress = "$ipStartRange$device"
+//                Callable { ScanResult(ipAddress = ipAddress, up = ping(ipAddress), scannedAt = LocalDateTime.now()) }
+//            }
+//            .map { service.submit(it) }
+//            .map { it.get() }
+//
+//    service.shutdown()
+
+    // return results
 }
 
 fun findBroadcastAddresses() = NetworkInterface.getNetworkInterfaces().toList()
@@ -45,6 +73,5 @@ fun scan(): List<ScanResult> {
 
     println("broadcastAddressesToScan: $broadcastAddressesToScan")
 
-    return broadcastAddressesToScan.flatMap { pingNetwork(it) }
-            .filter { it.up }
+    return broadcastAddressesToScan.flatMap { pingNetwork(it) }.filter { it.up }
 }
